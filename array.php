074bb716d18e0e1efb28063b01a6f95659d7c596
rakibul158 <rakib3665@gmail.com>
2021-09-10 00:07:18 +0600

    add for loop and for each loop

--- array.php
+++ array.php
@@ -1,8 +1,45 @@
 <?php
-    $car = [
+    echo "<b> For Loop Array Count</b>";
+    echo "<br>";
+    $cars = [
         "Valvo",
         "Toyta",
         "X-Corola"
     ];
-    print_r ($car);
+    $arraylenght = count($cars);
+    for($x=0; $x<$arraylenght; $x++)
+    {
+        echo $cars[$x];
+        echo "<br>";
+    }
+    echo "<br>";
+
+    echo "<b> ForEach Array Count</b>";
+    echo "<br>";
+    foreach($cars as $car){
+        echo $car;
+        echo "<br>";
+    }
+
+    echo "<br>";
+    
+    $friends = [
+        [
+            "name" => 'Md Rakibul Hasan',
+            "age" => 24
+        ],
+        [
+            "name" => 'Md Tipu Farazi',
+            "age" => 24
+        ],
+        [
+            "name" => 'Shahriar Alam Sajib',
+            "age" => 24
+        ]
+    ];
+
+    foreach($friends as $friend)
+    {
+        echo "Name: ".$friend['name']."<br>Age: ".$friend['age']."<br>";
+    }
 ?>
